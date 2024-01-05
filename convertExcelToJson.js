13a313dfdd083f4b1b5f77283f0462d31cf38742
Guillermo <guillermo@jelpus.com>
2024-01-05 21:05:36 +0100

    actualizacion script

--- convertExcelToJson.js
+++ convertExcelToJson.js
@@ -3,17 +3,6 @@ const csv = require('csv-parser');
 const axios = require('axios');
 const { Readable } = require('stream');
 
-let excelEjemplo = 'https://07109f54-7493-4017-b768-8102e95cfb89.usrfiles.com/ugd/07109f_40fb214d7f0c45b897903965263e92ed.xlsx'
-let csvEjemplo = 'https://07109f54-7493-4017-b768-8102e95cfb89.usrfiles.com/ugd/07109f_2f59dd6227194e92a53984e29b1a3be6.csv'
-
-const url = process.argv[2];
-
-if (!url) {
-    console.error('Por favor, proporciona una URL como argumento.');
-    process.exit(1);
-}
-
-
 async function streamToBuffer(stream) {
     const chunks = [];
     for await (const chunk of stream) {
@@ -22,14 +11,14 @@ async function streamToBuffer(stream) {
     return Buffer.concat(chunks);
 }
 
-async function convertExcelToJson(url) {
+async function convertExcelToJson(fileUrl) { // La función ahora acepta fileUrl como parámetro
     const response = await axios({
         method: 'get',
-        url: url,
+        url: fileUrl,
         responseType: 'stream'
     });
 
-    const format = url.split('.').pop().toLowerCase();
+    const format = fileUrl.split('.').pop().toLowerCase();
     let data = [];
 
     if (format === 'xlsx') {
@@ -45,12 +34,10 @@ async function convertExcelToJson(url) {
                 if (rowNumber === 1) {
                     headers[colNumber] = cell.value;
                 } else {
-                    // Verificar si el valor de la celda es numérico y manejarlo adecuadamente
                     const cellValue = cell.value;
                     if (cell.type === ExcelJS.ValueType.Number) {
                         rowData[headers[colNumber]] = cellValue;
                     } else if (typeof cellValue === 'object' && cellValue.result !== undefined) {
-                        // Para fórmulas, puedes decidir cómo manejar el resultado
                         rowData[headers[colNumber]] = cellValue.result;
                     } else {
                         rowData[headers[colNumber]] = cellValue;
@@ -81,9 +68,4 @@ async function convertExcelToJson(url) {
     return data;
 }
 
-convertExcelToJson(url).then(data => {
-    console.log(JSON.stringify(data, null, 2));
-}).catch(error => {
-    console.error('Error:', error);
-});
-
+module.exports = convertExcelToJson; // Exporta la función para usarla en otros archivos
